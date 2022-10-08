69800d9040c729c3f9cc85e727ed72568e01ae48
hehysh <89674984+hehysh@users.noreply.github.com>
2022-10-08 18:01:43 +0800

    Update settings.py

--- hexo_circle_of_friends/settings.py
+++ hexo_circle_of_friends/settings.py
@@ -17,13 +17,13 @@
 # 支持配置多个友链页面并指定不同主题策略，每个用{}分隔，它们会被同时爬取，数据保存在一起。***至少配置一个***
 LINK = [
     {
-        "link": "https://https://yisous.xyz/",  # 友链页地址1，修改为你的友链页地址
+        "link": "https://yisous.xyz/",  # 友链页地址1，修改为你的友链页地址
         "theme": "butterfly"
     },
-    #     {
-    #     "link": "https://noionion.top/link/",  # 友链页地址2
-    #     "theme": "butterfly",  # 友链页的获取策略
-    # },
+         {
+         "link": "https://blog.pai233.top/",  # 友链页地址2
+         "theme": "butterfly",  # 友链页的获取策略
+     },
     #     {
     #     "link": "https://immmmm.com/about/",  # 友链页地址3
     #     "theme": "common",  # 友链页的获取策略
